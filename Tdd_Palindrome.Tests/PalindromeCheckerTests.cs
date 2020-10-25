6a5fbcfd6678b139e7c205cc471efa9c0471cb2b
Rina Lingad <pixie.czar@gmail.com>
2020-10-25 16:20:08 +1300

    Create a test with build error because no code was added yet

---
+++ Tdd_Palindrome.Tests/PalindromeCheckerTests.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+namespace Tdd_Palindrome.Tests
+{
+    public class PalindromeCheckerTests
+    {
+        [Fact]
+        public void Given_one_character_then_palindrome()
+        {
+            var sut = new PalindromeChecker();
+            var result = sut.IsPalindrome('a');
+            result.Should().BeTrue();
+        }
+    }
+}
