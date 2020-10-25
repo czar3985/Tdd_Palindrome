8b144abb1d161e8751a4f17b32240ae39bbd18ff
Rina Lingad <pixie.czar@gmail.com>
2020-10-25 16:22:36 +1300

    Fix test build errors but tests are failing because function throws NotImplementedException

--- Tdd_Palindrome.Tests/PalindromeCheckerTests.cs
+++ Tdd_Palindrome.Tests/PalindromeCheckerTests.cs
@@ -1,3 +1,4 @@
+using FluentAssertions;
 using Xunit;
 
 namespace Tdd_Palindrome.Tests
---
+++ Tdd_Palindrome/PalindromeChecker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tdd_Palindrome
+{
+    public class PalindromeChecker
+    {
+        public bool IsPalindrome(char v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
